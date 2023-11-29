ea816640a1246c5de98444e651768248b9dca08f
prasenjit1980 <prasenjit1980@gmail.com>
2023-11-29 09:39:16 +0530

    Added 3 new ML model code

--- model.py
+++ model.py
@@ -1,1 +1,10 @@
 print("Adding modelling algorithms")
+
+def model1():
+    print("adding code for model 1")
+
+def model2():
+    print("adding code for model 2")
+    
+def model3():
+    print("adding code for model 3")    
